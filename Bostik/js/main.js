69350f0d024dc5e3d07a784d7174ef8e489aac93
daniaminakov <49240824+daniaminakov@users.noreply.github.com>
2019-07-31 12:26:47 +0300

    Add files via upload

---
+++ Bostik/js/main.js
@@ -0,0 +1,12 @@
+$(window).scroll(function(){
+
+    if ($(this).scrollTop() > 100) {
+        $('.scrollup').fadeIn();
+    } else {
+        $('.scrollup').fadeOut();
+    }
+});
+$('.scrollup').click(function(){
+    $("html, body").animate({ scrollTop: 0 }, 1000);
+    return false;
+});
