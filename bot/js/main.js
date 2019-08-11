ff942c8a9e3edae213862660dc1310fce712cc29
daniaminakov <49240824+daniaminakov@users.noreply.github.com>
2019-08-11 12:51:11 +0300

    Add files via upload

---
+++ bot/js/main.js
@@ -0,0 +1,15 @@
+
+// let bots = document.querySelector('.bots');
+// let arrow1 = document.querySelector('.arrow1');
+// let bots_block = document.querySelector('.bots-block');
+
+// bots.onclick = function(){
+// 	bots.style.color = "#329AD6";
+// 	arrow1.style.transform = "rotate(90deg)";
+// 	arrow1.style.right = "-20px";
+// 	bots_block.style.display = "flex";
+// }
+
+// // function navBot(){
+	
+// // }
