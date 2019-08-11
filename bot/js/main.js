4008a6301fd2be19fcc055d44e279b7b46432e6e
daniaminakov <49240824+daniaminakov@users.noreply.github.com>
2019-08-11 19:04:09 +0300

    Update main.js

--- bot/js/main.js
+++ bot/js/main.js
@@ -1,15 +1,36 @@
+jQuery(function($){
 
-// let bots = document.querySelector('.bots');
-// let arrow1 = document.querySelector('.arrow1');
-// let bots_block = document.querySelector('.bots-block');
+	$('.bots').on('click', function(e){
+		e.preventDefault;
+		$(this).toggleClass('bots-active');
+		$('.bots-block').toggleClass('bots-block-active');
 
-// bots.onclick = function(){
-// 	bots.style.color = "#329AD6";
-// 	arrow1.style.transform = "rotate(90deg)";
-// 	arrow1.style.right = "-20px";
-// 	bots_block.style.display = "flex";
-// }
+		$(document).mouseup(function (e){ 
+			const div = $('.bots');
+			if (!div.is(e.target)&& div.has(e.target).length === 0) {
+				$('.bots').removeClass('bots-active');
+				$('.bots-block').removeClass('bots-block-active');
+			}
+		});
+	});
 
-// // function navBot(){
-	
-// // }
+	$('.support').on('click', function(e){
+		e.preventDefault;
+		$(this).toggleClass('support-active');
+		$('.support-block').toggleClass('support-block-active');
+
+		$(document).mouseup(function (e){ 
+			const div = $('.support');
+			if (!div.is(e.target)&& div.has(e.target).length === 0) {
+				$('.support').removeClass('support-active');
+				$('.support-block').removeClass('support-block-active')
+			}
+		});
+	});
+
+
+
+});
+
+
+
