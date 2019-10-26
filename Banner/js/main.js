bc08d7e032698de56d9a558e0cafbf65a990c7e5
daniaminakov <49240824+daniaminakov@users.noreply.github.com>
2019-10-26 13:27:33 +0300

    Add files via upload

---
+++ Banner/js/main.js
@@ -0,0 +1,22 @@
+
+// Owl-carousel
+$('.owl-carousel').owlCarousel({
+    loop:true,
+    nav:true,
+    dots: false,
+    navText : ["",""],
+    autoplay:true,
+    autoplayTimeout:10000,
+    autoplayHoverPause:true,
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:1
+        },
+        1000:{
+            items:1
+        }
+    }
+})
