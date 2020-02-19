560ab2df05eb9aacae7826808e1f0280ba004970
daniaminakov <49240824+daniaminakov@users.noreply.github.com>
2020-02-19 21:41:02 +0200

    Add files via upload

--- KASSA24/js/main.js
+++ KASSA24/js/main.js
@@ -39,6 +39,18 @@ $(".btn").on("click", function (event) {
     $('body,html').animate({scrollTop: top}, 1000);
 });
 
+// Еще клик по ссылке
+$(".get-programm-content").on("click","a", function (event) {
+    //отменяем стандартную обработку нажатия по ссылке
+    event.preventDefault();
+    //забираем идентификатор блока с атрибута href
+    var id  = $(this).attr('href'),
+    //узнаем высоту от начала страницы до блока на который ссылается якорь
+    top = $(id).offset().top;
+    //анимируем переход на расстояние - top за 1000 мс
+    $('body,html').animate({scrollTop: top}, 1000);
+});
+
 
 // terminal-carousel-START
 $('.terminal-slider .owl-carousel').owlCarousel({
