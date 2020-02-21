4f58defe7772d24603a31b053b0a771a9d5a63ff
daniaminakov <49240824+daniaminakov@users.noreply.github.com>
2020-02-21 14:57:49 +0200

    Add files via upload

--- KASSA24/js/main.js
+++ KASSA24/js/main.js
@@ -16,7 +16,19 @@ $('.scrollup').click(function(){
 
 
 // Клики по навигации меню
-$(".navigation").on("click","a", function (event) {
+$(".menu").on("click","a", function (event) {
+    //отменяем стандартную обработку нажатия по ссылке
+    event.preventDefault();
+    //забираем идентификатор блока с атрибута href
+    var id  = $(this).attr('href'),
+    //узнаем высоту от начала страницы до блока на который ссылается якорь
+    top = $(id).offset().top;
+    //анимируем переход на расстояние - top за 1000 мс
+    $('body,html').animate({scrollTop: top}, 1000);
+});
+
+// Клики по навигации меню
+$(".small-menu").on("click","a", function (event) {
     //отменяем стандартную обработку нажатия по ссылке
     event.preventDefault();
     //забираем идентификатор блока с атрибута href
