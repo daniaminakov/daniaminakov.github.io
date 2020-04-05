959aa5ddeb141fd65b89c38cfd55f68476f85f13
Danil <danilminakov333@gmail.com>
2020-04-05 22:05:29 +0300

    7

--- Portfolio/js/main.js
+++ Portfolio/js/main.js
@@ -80,7 +80,7 @@ $(".arrow-bottom").on("click", function(event) {
 // Прогрес-скиллы-START
 function progressSkills() {
     var progressBar = document.querySelectorAll(".progress-bar");
-    var time = 2000;
+    var time = 3500;
     progressBar.forEach(function(i) {
         let label = i.children[1].children[0].children[0];
         let line = i.children[1];
