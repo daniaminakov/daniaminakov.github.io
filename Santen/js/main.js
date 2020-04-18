455e553315f20d06df9788cb1f023e822f3bbdfb
Danil <danilminakov333@gmail.com>
2020-04-18 11:08:02 +0300

    18.04.2020

--- Santen/js/main.js
+++ Santen/js/main.js
@@ -1,6 +1,19 @@
-// Клик по бургер-меню-START
-function menuClick() {
-    this.classList.toggle('menu-opened');
-}
-document.querySelector('.burger-menu').addEventListener('click', menuClick);
-// Клик по бургер-меню-END
+jQuery(document).ready(function($) {
+
+    // Клик по бургер-меню-START
+    function menuClick() {
+        this.classList.toggle('menu-opened');
+    }
+    document.querySelector('.burger-menu').addEventListener('click', menuClick);
+    // Клик по бургер-меню-END
+
+    // Слайдер
+    $('.slider').slick({
+        arrows: true,
+        dots: false,
+        slidesToShow: 3,
+        infinite: true,
+        speed: 300
+    });
+
+});
