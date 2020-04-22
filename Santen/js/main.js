0c75a0e44904cabffe86954e1c5099cb51960713
Danil <danilminakov333@gmail.com>
2020-04-22 18:56:12 +0300

    js

--- Santen/js/main.js
+++ Santen/js/main.js
@@ -13,7 +13,7 @@ jQuery(document).ready(function($) {
     // scrollup-START
     $(window).scroll(function() {
 
-        if ($(this).scrollTop() > 1000) {
+        if ($(this).scrollTop() > 500) {
             $('.scrollup').fadeIn();
         } else {
             $('.scrollup').fadeOut();
@@ -27,7 +27,7 @@ jQuery(document).ready(function($) {
 
 
 
-    // Слайдер
+    // Слайдер1
     $('.slider').slick({
         arrows: true,
         dots: false,
@@ -36,65 +36,83 @@ jQuery(document).ready(function($) {
         speed: 300
     });
 
-});
-
-// Клики по кнопкам PAGE - Синдром сухого глаза - меню
-$(".syndrome-menu-wrapper").on("click", "a", function(event) {
-    //отменяем стандартную обработку нажатия по ссылке
-    event.preventDefault();
-    //забираем идентификатор блока с атрибута href
-    var id = $(this).attr('href'),
-        //узнаем высоту от начала страницы до блока на который ссылается якорь
-        top = $(id).offset().top;
-    //анимируем переход на расстояние - top за 1000 мс
-    $('body,html').animate({ scrollTop: top }, 1500);
-});
-
-// Клики по кнопкам PAGE - Синдром сухого глаза - Кто в группе риска?
-$(".box-right .blue").on("click", "a", function(event) {
-    //отменяем стандартную обработку нажатия по ссылке
-    event.preventDefault();
-    //забираем идентификатор блока с атрибута href
-    var id = $(this).attr('href'),
-        //узнаем высоту от начала страницы до блока на который ссылается якорь
-        top = $(id).offset().top;
-    //анимируем переход на расстояние - top за 1000 мс
-    $('body,html').animate({ scrollTop: top }, 1500);
-});
-
-
-// Test
-$('#testBtn1').on("click", function() {
-    $('.question1').removeClass("question-active");
-    $('.question2').addClass("question-active");
-});
-
-$('#testBtn2').on("click", function() {
-    $('.question1').removeClass("question-active");
-    $('.question3').addClass("question-active");
-});
-
-$('#testBtn3').on("click", function() {
-    $('.question2').removeClass("question-active");
-    $('.question4').addClass("question-active");
-});
-
-$('#testBtn4').on("click", function() {
-    $('.question3').removeClass("question-active");
-    $('.question5').addClass("question-active");
-});
-
-$('#testBtn5').on("click", function() {
-    $('.question5').removeClass("question-active");
-    $('.question6').addClass("question-active");
-});
-
-$('.question-btn2').on("click", function() {
-    $('.test-wrapper .question').each(function() {
-        if ($(this).hasClass("question-active")) {
-            this.classList.remove("question-active");
-            $('.question1').addClass("question-active");
-        }
-
+    // Слайдер2
+    $('.product-slider').slick({
+        arrows: true,
+        dots: false,
+        slidesToShow: 1,
+        infinite: true,
+        speed: 300
     });
+
+
+    // Клики по кнопкам PAGE - Синдром сухого глаза - меню
+    $(".syndrome-menu-wrapper").on("click", "a", function(event) {
+        //отменяем стандартную обработку нажатия по ссылке
+        event.preventDefault();
+        //забираем идентификатор блока с атрибута href
+        var id = $(this).attr('href'),
+            //узнаем высоту от начала страницы до блока на который ссылается якорь
+            top = $(id).offset().top;
+        //анимируем переход на расстояние - top за 1000 мс
+        $('body,html').animate({ scrollTop: top }, 1500);
+    });
+
+    // Клики по кнопкам PAGE - Синдром сухого глаза - Кто в группе риска?
+    $(".box-right .blue").on("click", "a", function(event) {
+        //отменяем стандартную обработку нажатия по ссылке
+        event.preventDefault();
+        //забираем идентификатор блока с атрибута href
+        var id = $(this).attr('href'),
+            //узнаем высоту от начала страницы до блока на который ссылается якорь
+            top = $(id).offset().top;
+        //анимируем переход на расстояние - top за 1000 мс
+        $('body,html').animate({ scrollTop: top }, 1500);
+    });
+
+
+    // Test
+    $('#testBtn1').on("click", function() {
+        $('.question1').removeClass("question-active");
+        $('.question2').addClass("question-active");
+    });
+
+    $('#testBtn2').on("click", function() {
+        $('.question1').removeClass("question-active");
+        $('.question3').addClass("question-active");
+    });
+
+    $('#testBtn3').on("click", function() {
+        $('.question2').removeClass("question-active");
+        $('.question4').addClass("question-active");
+    });
+
+    $('#testBtn4').on("click", function() {
+        $('.question3').removeClass("question-active");
+        $('.question5').addClass("question-active");
+    });
+
+    $('#testBtn5').on("click", function() {
+        $('.question5').removeClass("question-active");
+        $('.question6').addClass("question-active");
+    });
+
+    $('.question-btn2').on("click", function() {
+        $('.test-wrapper .question').each(function() {
+            if ($(this).hasClass("question-active")) {
+                this.classList.remove("question-active");
+                $('.question1').addClass("question-active");
+            }
+
+        });
+    });
+
+
+
+    // instruction-product
+    $('.instruction-item__title').on('click', function() {
+        $(this).parent().toggleClass('instruction-active');
+        return false;
+    });
+
 });
