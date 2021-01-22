07e1642e0e2c5b9e5ab3d25884c845f0c99940c1
daniaminakov <49240824+daniaminakov@users.noreply.github.com>
2021-01-22 16:59:37 +0200

    Update main.js

--- DinnerLab/js/main.js
+++ DinnerLab/js/main.js
@@ -37,7 +37,7 @@ $( document ).ready(function() {
 
 
 	// Clock-START
-	var futureDate  = new Date("Jan 23 2021 12:00:00");
+	var futureDate  = new Date("Jan 24 2021 12:00:00");
 	var currentDate = new Date();
 	// Calculate the difference in seconds between the future and current date
 	var diff = futureDate.getTime() / 1000 - currentDate.getTime() / 1000;
